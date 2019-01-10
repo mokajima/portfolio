c40fa6bfa91019c909ba1c078466381041ab2641
mokajima <mokajima.85z@gmail.com>
2019-01-10 22:41:03 +0900

    fix: Use target="_blank" with rel="noopener noreferrer"

--- src/components/Work.js
+++ src/components/Work.js
@@ -6,13 +6,13 @@ const Work = (props) => {
 
   return (
     <div className="work">
-      <a href={work.url} target="_blank">
+      <a href={work.url} target="_blank" rel="noopener noreferrer">
         <figure className={className}>
           <img src={`images/${work.id}.jpg`} alt={work.title} />
         </figure>
       </a>
       <h3 className="work__title">
-        <a href={work.url} target="_blank">{work.title}</a>
+        <a href={work.url} target="_blank" rel="noopener noreferrer">{work.title}</a>
       </h3>
       <p className="work__summary">{work.summary}</p>
     </div>
