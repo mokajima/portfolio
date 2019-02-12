de57e58e20e43cf369153da36bbc657f46c83658
mokajima <mokajima.85z@gmail.com>
2019-02-12 23:22:45 +0900

    style: Use an arrow function instead of a function declaration

--- src/components/WorksList.js
+++ src/components/WorksList.js
@@ -2,20 +2,18 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Work from './Work'
 
-function WorksList(props) {
-  return (
-    <section className="section">
-      <h2 className="headline">Featured Work</h2>
-      <ul className="featured-work">
-        {props.works.map(work => (
-          <li key={work.id}>
-            <Work work={work} />
-          </li>
-        ))}
-      </ul>
-    </section>
-  )
-}
+const WorksList = props => (
+  <section className="section">
+    <h2 className="headline">Featured Work</h2>
+    <ul className="featured-work">
+      {props.works.map(work => (
+        <li key={work.id}>
+          <Work work={work} />
+        </li>
+      ))}
+    </ul>
+  </section>
+)
 
 WorksList.propTypes = {
   works: PropTypes.array.isRequired
