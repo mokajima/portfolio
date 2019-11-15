1d699b566938830d41572ce9b1aa6fea096c5a29
mokajima <mokajima85z@gmail.com>
2019-11-15 23:15:19 +0900

    refactor: Refactor the App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,45 +5,42 @@ import WorksListContainer from './containers/WorksListContainer'
 import './App.css'
 import header from './header.jpg'
 
-const App: FC = () => {
+const year = new Date().getFullYear()
 
-  const year = new Date().getFullYear()
-
-  return (
-    <>
-      <header className="header">
-        <h1 className="header__logo">Misaki's Portfolio Website</h1>
+const App: FC = () => (
+  <>
+    <header className="header">
+      <h1 className="header__logo">Misaki's Portfolio Website</h1>
+    </header>
+    <main className="main">
+      <header className="hero">
+        <h2 className="hero__title">Hello, I'm Misaki!</h2>
+        <img className="hero__img" src={header} alt="" />
       </header>
-      <main className="main">
-        <header className="hero">
-          <h2 className="hero__title">Hello, I'm Misaki!</h2>
-          <img className="hero__img" src={header} alt="" />
-        </header>
-        <WorksListContainer />
-      </main>
-      <footer className="footer">
-        <div className="footer__inner profile">
-          <h2 className="headline">About Me</h2>
-          <p className="profile__bio">Hello, I'm Misaki, a front end developer, based in Japan.</p>
-          <ul className="footer__nav">
-            <li className="footer__nav-item">
-              <a href="https://github.com/mokajima/">
-                <FontAwesomeIcon icon={faGithub} size="2x" />
-              </a>
-            </li>
-            <li className="footer__nav-item">
-              <a href="https://twitter.com/mokajima85z">
-                <FontAwesomeIcon icon={faTwitter} size="2x" />
-              </a>
-            </li>
-          </ul>
-        </div>
-        <p className="copyright">
-          <small>&copy; {year} Misaki's Portfolio Website</small>
-        </p>
-      </footer>
-    </>
-  )
-}
+      <WorksListContainer />
+    </main>
+    <footer className="footer">
+      <div className="footer__inner profile">
+        <h2 className="headline">About Me</h2>
+        <p className="profile__bio">Hello, I'm Misaki, a front end developer, based in Japan.</p>
+        <ul className="footer__nav">
+          <li className="footer__nav-item">
+            <a href="https://github.com/mokajima/">
+              <FontAwesomeIcon icon={faGithub} size="2x" />
+            </a>
+          </li>
+          <li className="footer__nav-item">
+            <a href="https://twitter.com/mokajima85z">
+              <FontAwesomeIcon icon={faTwitter} size="2x" />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <p className="copyright">
+        <small>&copy; {year} Misaki's Portfolio Website</small>
+      </p>
+    </footer>
+  </>
+)
 
 export default App
